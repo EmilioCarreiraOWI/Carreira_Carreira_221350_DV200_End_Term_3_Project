3825bc7e9d96f1bb99bc6243d77196bd8f894a5f
EmilioCarreiraOWI <221350@virtualwindow.co.za>
2023-08-02 14:11:37 +0200

    progress

--- emiliocarreira-term-three/src/componets/Nav.js
+++ emiliocarreira-term-three/src/componets/Nav.js
@@ -1,28 +1,27 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
-import Logo from './game_cloud_logo.jpg';
+
 
 function BasicNav() {
 
     return(
         <nav class="navbar navbar-expand-lg navbar-light fixed-top">
             <div class="container">
-            <Logo /> SpaceX
-                <a class="navbar-brand headings" href="./">Game Cloud</a>
+            <a class="navbar-brand headings" href="./">Game Cloud</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto">
+                    <ul class="navbar-nav ml-auto headings">
                         <li class="nav-item active">
                             <a class="nav-link" href="./">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="../pages/More.js">More Games</a>
+                            <a class="nav-link" href="./More.js">More Games</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="../pages/Library.js">Librery</a>
+                            <a class="nav-link" href="./Library.js">Librery</a>
                         </li>
                     </ul>
                 </div>
--- emiliocarreira-term-three/src/pages/Landing.js
+++ emiliocarreira-term-three/src/pages/Landing.js
@@ -2,34 +2,24 @@ import React from 'react';
 import '../App'; 
 import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
+function Landing() {
     return (
       <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          <li data-target="#slide1" data-slide-to="0" class="active"></li>
+          <li data-target="#slide2" data-slide-to="1"></li>
+          <li data-target="#slide3" data-slide-to="2"></li>
         </ol>
 
-        <div class="carousel-inner">
+        <div class="carousel-inner mt-5">
           <div class="carousel-item active">
-            <img class="d-block w-100" src="https://s3-alpha-sig.figma.com/img/9425/546e/f3ccc5fc14d8fe67c568ae945491a086?Expires=1691971200&Signature=pLriK5dwE~UJUGFjIrRyYirL8z7n~EQnFvyz-zzj0kA9UQIh5EO1~aNvPpLCC6pGpjQkViWjmR23Xhx5M~cK6M1fpvrF8-5qk1onLHX5Rr0MBj990Vi91jBJnxacKTkao-zyrFPvNnMWluTYCXJC5yLxhk1QyJOYBK-szL2TvhDzrfuzUOZ1sJK~GnhugVYeWqLVbEDizc9JW5DFVr2NxH2ypuREkIkoJZP1cXiEeIQ9GoBWr8ojEIO9~Wtt~0-B2pXkX77Lbfw2igdDin4mF6dfwCSxdStfrFnOuKlvoZjU-6zAKmYJpn-GydlMwPMRBA5UgWJ6hWoLrqRQosUxNw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="First slide"></img>
+            
             <div class="carousel-caption d-none d-md-block">
-              <h5>Slider One Item</h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, nulla, tempore. Deserunt excepturi quas vero.</p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="https://i.postimg.cc/pVzg3LWn/2.jpg" alt="Second slide"></img>
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Slider One Item</h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, nulla, tempore. Deserunt excepturi quas vero.</p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="https://i.postimg.cc/0y2F6Gpp/3.jpg" alt="Third slide"></img>
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Slider One Item</h5>
+              <div>
+                <img src="https://s3-alpha-sig.figma.com/img/9d5c/3b6f/867fdd9fec280218ea29f8a263357829?Expires=1691971200&Signature=lKSu4c-~LDYbckvW3kKWBrc0L91CKpRAJdt8~ITme3gJ4GtAEhq1TtCA-pXmGYC9h9RZLXJhBlavk5FZjoV0huZ2MdOzbNSaA3YxoOejm3zlLvG4Vq7ulU6SSkn0u~kgiujDD5qjodiGWs0stg39kCjjVJNtsNidmIe0PILC0ETz0BH4wxeiK8XORNctaURYLo7FvDlP6JFWdr~SDOs6aS~s6SkiwW198aoOiRyPRcmnlAEFcF2V3pUw4rhEBtFmgoUlhXSE60ADn8N4hGNALiferNG1efhoGiEZDuG~4sWWilhzSsLy7TEMmnC1LuAzOxHJVMDmLdFV8APm6M-YDA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" class="bigImg" alt='placeholder-bigImg'></img>
+              </div>
+              
+              <h5>Game Name</h5>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, nulla, tempore. Deserunt excepturi quas vero.</p>
             </div>
           </div>
@@ -37,15 +27,15 @@ function App() {
       
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
+          <span class="sr-only"></span>
         </a>
 
         <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
+          <span class="sr-only"></span>
         </a>
       </div>
     );
   }
   
-  export default App;
+  export default Landing;
