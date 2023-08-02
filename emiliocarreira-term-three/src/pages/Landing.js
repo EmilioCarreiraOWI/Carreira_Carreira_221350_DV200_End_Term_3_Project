742c7fb892f51c9d8130d24feedf4e19b4f8120d
EmilioCarreiraOWI <221350@virtualwindow.co.za>
2023-08-02 21:05:22 +0200

    progress

--- emiliocarreira-term-three/src/pages/Landing.js
+++ emiliocarreira-term-three/src/pages/Landing.js
@@ -4,31 +4,27 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function Landing() {
     return (
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner mt-5">
-          <div class="carousel-item active">
-            
-            <div class="carousel-caption d-none d-md-block">
-              <div>
-                <img src="https://s3-alpha-sig.figma.com/img/9d5c/3b6f/867fdd9fec280218ea29f8a263357829?Expires=1691971200&Signature=lKSu4c-~LDYbckvW3kKWBrc0L91CKpRAJdt8~ITme3gJ4GtAEhq1TtCA-pXmGYC9h9RZLXJhBlavk5FZjoV0huZ2MdOzbNSaA3YxoOejm3zlLvG4Vq7ulU6SSkn0u~kgiujDD5qjodiGWs0stg39kCjjVJNtsNidmIe0PILC0ETz0BH4wxeiK8XORNctaURYLo7FvDlP6JFWdr~SDOs6aS~s6SkiwW198aoOiRyPRcmnlAEFcF2V3pUw4rhEBtFmgoUlhXSE60ADn8N4hGNALiferNG1efhoGiEZDuG~4sWWilhzSsLy7TEMmnC1LuAzOxHJVMDmLdFV8APm6M-YDA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" class="bigImg" alt='placeholder-bigImg'></img>
-              </div>
-              
-              <h5>Cyberpunk</h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, nulla, tempore. Deserunt excepturi quas vero.</p>
-            </div>
-          </div>
-        </div>
-      
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only"></span>
-        </a>
-
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only"></span>
-        </a>
-      </div>
+      <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="https://i.pinimg.com/564x/83/e8/ad/83e8addf549ba7274e90cd192006ec95.jpg" class="d-block w-100" alt="..."></img>
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="..." class="d-block w-100" alt="..."></img>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="..."></img>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
     );
   }
   
--- emiliocarreira-term-three/src/pages/More.js
+++ emiliocarreira-term-three/src/pages/More.js
@@ -4,37 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function More() {
     return (
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#slide1" data-slide-to="0" class="active"></li>
-          <li data-target="#slide2" data-slide-to="1"></li>
-          <li data-target="#slide3" data-slide-to="2"></li>
-        </ol>
-
-        <div class="carousel-inner mt-5">
-          <div class="carousel-item active">
-            
-            <div class="carousel-caption d-none d-md-block">
-              <div>
-                <img src="https://s3-alpha-sig.figma.com/img/9d5c/3b6f/867fdd9fec280218ea29f8a263357829?Expires=1691971200&Signature=lKSu4c-~LDYbckvW3kKWBrc0L91CKpRAJdt8~ITme3gJ4GtAEhq1TtCA-pXmGYC9h9RZLXJhBlavk5FZjoV0huZ2MdOzbNSaA3YxoOejm3zlLvG4Vq7ulU6SSkn0u~kgiujDD5qjodiGWs0stg39kCjjVJNtsNidmIe0PILC0ETz0BH4wxeiK8XORNctaURYLo7FvDlP6JFWdr~SDOs6aS~s6SkiwW198aoOiRyPRcmnlAEFcF2V3pUw4rhEBtFmgoUlhXSE60ADn8N4hGNALiferNG1efhoGiEZDuG~4sWWilhzSsLy7TEMmnC1LuAzOxHJVMDmLdFV8APm6M-YDA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" class="bigImg" alt='placeholder-bigImg'></img>
-              </div>
-              
-              <h5>Game Name</h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, nulla, tempore. Deserunt excepturi quas vero.</p>
-            </div>
-          </div>
-        </div>
-      
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only"></span>
-        </a>
-
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only"></span>
-        </a>
-      </div>
+      <p>hello</p>
     );
   }
   
