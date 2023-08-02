6ee82a63c48d08f31e6a034fbbbeff8ae7fc593b
EmilioCarreiraOWI <221350@virtualwindow.co.za>
2023-08-02 14:26:51 +0200

    progress

--- emiliocarreira-term-three/src/componets/Nav.js
+++ emiliocarreira-term-three/src/componets/Nav.js
@@ -5,8 +5,34 @@ import 'bootstrap/dist/css/bootstrap.css';
 function BasicNav() {
 
     return(
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-black">
             <div class="container">
+                <svg width="42" height="43" viewBox="0 0 72 73" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect width="72" height="73" fill="#E5E5E5"/>
+                <g id="Group 31">
+                <g id="Group 27">
+                <rect id="Rectangle 333" x="-98" y="-31" width="1868.42" height="2494" fill="#1C1C1C"/>
+                <g id="Rectangle 257">
+                <mask id="path-2-inside-1_0_1" fill="white">
+                <path d="M-98 -31H1770.42V101H-98V-31Z"/>
+                </mask>
+                <path d="M-98 -31H1770.42V101H-98V-31Z" fill="#111111"/>
+                <path d="M1770.42 96H-98V106H1770.42V96Z" fill="white" mask="url(#path-2-inside-1_0_1)"/>
+                </g>
+                <g id="Group 26">
+                <path id="Ellipse 70" d="M70.3261 36.5C70.3261 56.132 54.8457 72 35.8066 72C16.7675 72 1.28711 56.132 1.28711 36.5C1.28711 16.868 16.7675 1 35.8066 1C54.8457 1 70.3261 16.868 70.3261 36.5Z" fill="#A26337" stroke="white" stroke-width="2"/>
+                <ellipse id="Ellipse 71" cx="26.3754" cy="36.8242" rx="19.1742" ry="13.5664" fill="#111111"/>
+                <ellipse id="Ellipse 72" cx="18.8619" cy="13.242" rx="18.8619" ry="13.242" transform="matrix(0.998004 0.0631493 -0.0598145 0.99821 26.8906 26.6133)" fill="#111111"/>
+                <ellipse id="Ellipse 73" cx="38.0071" cy="27.7782" rx="11.9446" ry="12.2743" fill="#111111"/>
+                <rect id="Rectangle 313" x="26.6914" y="31.6562" width="17.6026" height="6.46018" fill="#D9D9D9"/>
+                <ellipse id="Ellipse 74" cx="4.41435" cy="6.76313" rx="4.41435" ry="6.76313" transform="matrix(0.939728 0.341923 -0.325771 0.945449 26.6973 28.4258)" fill="#D9D9D9"/>
+                <ellipse id="Ellipse 75" cx="4.41222" cy="6.76625" rx="4.41222" ry="6.76625" transform="matrix(0.949307 -0.31435 0.299218 0.954185 36.1191 31.1992)" fill="#D9D9D9"/>
+                <ellipse id="Ellipse 76" cx="31.4056" cy="35.8533" rx="1.57166" ry="1.61504" fill="#111111"/>
+                <ellipse id="Ellipse 77" cx="38.9506" cy="35.8533" rx="1.57166" ry="1.61504" fill="#111111"/>
+                </g>
+                </g>
+                </g>
+                </svg>
             <a class="navbar-brand headings" href="./">Game Cloud</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
--- emiliocarreira-term-three/src/pages/Landing.js
+++ emiliocarreira-term-three/src/pages/Landing.js
@@ -5,12 +5,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 function Landing() {
     return (
       <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#slide1" data-slide-to="0" class="active"></li>
-          <li data-target="#slide2" data-slide-to="1"></li>
-          <li data-target="#slide3" data-slide-to="2"></li>
-        </ol>
-
         <div class="carousel-inner mt-5">
           <div class="carousel-item active">
             
@@ -19,7 +13,7 @@ function Landing() {
                 <img src="https://s3-alpha-sig.figma.com/img/9d5c/3b6f/867fdd9fec280218ea29f8a263357829?Expires=1691971200&Signature=lKSu4c-~LDYbckvW3kKWBrc0L91CKpRAJdt8~ITme3gJ4GtAEhq1TtCA-pXmGYC9h9RZLXJhBlavk5FZjoV0huZ2MdOzbNSaA3YxoOejm3zlLvG4Vq7ulU6SSkn0u~kgiujDD5qjodiGWs0stg39kCjjVJNtsNidmIe0PILC0ETz0BH4wxeiK8XORNctaURYLo7FvDlP6JFWdr~SDOs6aS~s6SkiwW198aoOiRyPRcmnlAEFcF2V3pUw4rhEBtFmgoUlhXSE60ADn8N4hGNALiferNG1efhoGiEZDuG~4sWWilhzSsLy7TEMmnC1LuAzOxHJVMDmLdFV8APm6M-YDA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" class="bigImg" alt='placeholder-bigImg'></img>
               </div>
               
-              <h5>Game Name</h5>
+              <h5>Cyberpunk</h5>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, nulla, tempore. Deserunt excepturi quas vero.</p>
             </div>
           </div>
