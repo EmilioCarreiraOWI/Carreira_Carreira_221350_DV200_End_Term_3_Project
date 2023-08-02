b61390b82caed5cd8eade4e19f56144d151feb3f
EmilioCarreiraOWI <221350@virtualwindow.co.za>
2023-08-02 22:15:40 +0200

    progress
    carousel successful added

--- emiliocarreira-term-three/src/pages/Landing.js
+++ emiliocarreira-term-three/src/pages/Landing.js
@@ -1,30 +1,95 @@
 import React from 'react';
 import '../App'; 
 import 'bootstrap/dist/css/bootstrap.css';
+import { Carousel } from 'react-carousel-minimal';
+
+const data = [{
+          image: "https://cdn1.epicgames.com/rosemallow/offer/TOW_EGS_LandscapeProductImage_2560x1440-2560x1440-4f2adef2a0292ae2e3a91a7b6b076547.jpg",
+          caption: `<div>
+                      The outer worlds
+                      <br/>
+                      Next line
+                    </div>`
+        },
+        {
+          image: "https://w0.peakpx.com/wallpaper/1004/530/HD-wallpaper-games-game-poster.jpg",
+          caption: "Gears of war 4"
+        },
+        {
+          image: "https://cdn1.epicgames.com/clary/offer/GameName_Store_Landscape_2560x1440%20(1)-2560x1440-2456ec2b7ad7c1a093330e607965d985.jpg",
+          caption: "Assassin's Creed: Odyssey"
+        },
+        {
+          image: "https://i.pinimg.com/originals/a7/1e/70/a71e70f0be92196f8567e6806da06738.png",
+          caption: "Crysis 3"
+        },
+        {
+          image: "https://m.media-amazon.com/images/I/71HsBff1RfL.jpg",
+          caption: "Minecraft"
+        },
+        {
+          image: "https://wallpapercave.com/wp/wp7095300.jpg",
+          caption: "Horizon: Forbidden West"
+        },
+        {
+          image: "https://wallpapercave.com/wp/wp6072817.jpg",
+          caption: "Halo 5"
+        },
+        {
+          image: "https://cutewallpaper.org/28/cool-pc-video-game-wallpaper-hd/291372088.jpg",
+          caption: "Titanfall 2"
+        },
+        {
+          image: "https://images.hdqwalls.com/download/call-of-duty-modern-warfare-3-4k-do-1366x768.jpg",
+          caption: "Cakk of Duty: MW3"
+        }
+      ];
+    
+      const captionStyle = {
+        fontSize: '2em',
+        fontWeight: 'bold',
+      }
+      const slideNumberStyle = {
+        fontSize: '20px',
+        fontWeight: 'bold',
+      }
+
+      
 
 function Landing() {
     return (
-      <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="10000">
-      <img src="https://i.pinimg.com/564x/83/e8/ad/83e8addf549ba7274e90cd192006ec95.jpg" class="d-block w-100" alt="..."></img>
-    </div>
-    <div class="carousel-item" data-bs-interval="2000">
-      <img src="..." class="d-block w-100" alt="..."></img>
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="..."></img>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+        <div className="App">
+          <div style={{ textAlign: "center" }}>
+            <div>
+              <Carousel
+                data={data}
+                time={3000}
+                width="950px"
+                height="600px"
+                captionStyle={captionStyle}
+                radius="10px"
+                slideNumber={true}
+                slideNumberStyle={slideNumberStyle}
+                captionPosition="bottom"
+                automatic={true}
+                dots={true}
+                pauseIconColor="white"
+                pauseIconSize="40px"
+                slideBackgroundColor="darkgrey"
+                slideImageFit="cover"
+                thumbnails={true}
+                thumbnailWidth="100px"
+                style={{
+                  textAlign: "center",
+                  maxWidth: "950px",
+                  maxHeight: "600px",
+                  margin: "40px auto",
+                }}
+              />
+            </div>
+          </div>
+        </div>
+
     );
   }
   
