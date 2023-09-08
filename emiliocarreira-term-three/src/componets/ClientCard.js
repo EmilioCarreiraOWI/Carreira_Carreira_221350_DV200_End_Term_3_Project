cdf2ddc2e539a16f17204c3357d66322623867ec
EmilioCarreiraOWI <221350@virtualwindow.co.za>
2023-09-09 00:27:57 +0200

    up to date!!

--- emiliocarreira-term-three/src/componets/ClientCard.js
+++ emiliocarreira-term-three/src/componets/ClientCard.js
@@ -8,7 +8,7 @@ const ClientCard = ({ game }) => {
 
 
     return (
-        <Col id='testbox' lg={2} className='normal-text justify-content-center btn'>
+        <Col id='testbox' lg={2} className='normal-text justify-content-center btn mx-3 mt-4' to={`/Indevidual/${game._id}`}>
             <img className='card-img w-100 h-auto' src={game.image} alt={game.image}></img>
             <p className='text-center mt-2'>{game.name}</p>
         </Col>
