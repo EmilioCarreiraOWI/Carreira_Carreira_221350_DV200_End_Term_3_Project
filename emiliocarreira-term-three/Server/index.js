bc66bc78b2a2b5958d3fc6d33cc876f074a4d084
EmilioCarreiraOWI <221350@virtualwindow.co.za>
2023-09-09 08:09:48 +0200

    progress

--- emiliocarreira-term-three/Server/index.js
+++ emiliocarreira-term-three/Server/index.js
@@ -3,7 +3,9 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 // const { log } = require('console');
 const gameRoute = require('./routes/games');
-// const userRoute = require('./routes/user.js') 
+// const userRoute = require('./routes/user.js')
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");  
 
 require('dotenv/config');
 
@@ -29,6 +31,9 @@ mongoose.connect(process.env.DB_CONNECTION, {
 .catch((err) => {
     console.log("No Connection. Error:" + err);
 })
+
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => { console.log('Server started on port', PORT)})
