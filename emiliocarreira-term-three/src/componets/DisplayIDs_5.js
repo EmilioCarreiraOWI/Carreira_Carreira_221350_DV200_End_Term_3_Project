81c03ccf9ed118236f75e79a92a409ec1efc5340
EmilioCarreiraOWI <221350@virtualwindow.co.za>
2023-09-09 13:30:46 +0200

    sad progress

--- emiliocarreira-term-three/src/componets/DisplayIDs_5.js
+++ emiliocarreira-term-three/src/componets/DisplayIDs_5.js
@@ -26,20 +26,24 @@ const DisplayIDs_5 = () => {
       };
 
   return (
-    <Col md={2} id="testbox" className="btn m-3">
-                <img
-                    className='card-img w-100 h-auto'
-                    src={game.image}
-                    alt={game.image}
-                ></img>
-                <Link
-                    to='/Indevidual' // Navigate to the other page where you want to display the selected game ID
-                    className="btn btn-secondary text-center mt-2 w-100 btn-outline-light"
-                    onClick={() => handleSelectGame(game.id)}
-                >
-                    {game.name}
-                </Link>
+    <Col>
+      {game.map((game) => (
+        <Col md={2} id="testbox" className="btn m-3">
+              <img
+                  className='card-img w-100 h-auto'
+                  src={game.image}
+                  alt={game.image}
+              ></img>
+              <Link
+                  to='/Indevidual' // Navigate to the other page where you want to display the selected game ID
+                  className="btn btn-secondary text-center mt-2 w-100 btn-outline-light"
+                  onClick={() => handleSelectGame(game.id)}
+              >
+                  {game.name}
+              </Link>
         </Col>
+      ))}
+    </Col>
   );
 };
 
